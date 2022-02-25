78a2a93f7a3ab6e26111d8386094e3d245264331
Wyatt Kribs <kribswyatt@gmail.com>
2022-02-24 22:57:32 -0700

    added annotations

--- variables.rb
+++ variables.rb
@@ -1,17 +1,17 @@
 name = "Wyatt"
-
+#me myself
 partner = "Ash"
-
+#my boy, too pure for this world
 hometown = "Driggs"
 
 age = 27
 
 year = 2022
-
+#at least it's not 2021
 change_in_pocket = 0.10
-
+#subject to change
 braincells = 2.5
 
 sleepy = true
-
+#ready to kick tomorrow's ass
 lack_of_motivation = false
